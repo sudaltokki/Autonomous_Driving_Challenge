55ace424ffc23e07a6f5a7846f3b30cc8b5a5b3b
Yerim <137975664+mireyerim@users.noreply.github.com>
2024-09-27 11:35:40 +0900

    update

--- data_cleansing.py
+++ data_cleansing.py
@@ -1,5 +1,6 @@
 import cv2
 import os
+import numpy
 
 base_folder = r"C:\Users\YERIM\Source\autonomous challenge\data\train"
 
@@ -9,23 +10,29 @@ for subfolder in os.listdir(base_folder):
     if os.path.isdir(sub_path):
         img_path = os.path.join(sub_path, 'img')
         new_txt_path = os.path.join(sub_path, 'new_txt')
+        mask_path = os.path.join(sub_path, 'instance')
         cleansing_path = os.path.join(sub_path, 'cleansing')
 
         if not os.path.exists(cleansing_path):
             os.makedirs(cleansing_path)
             print('makedir ', cleansing_path)
 
-        classes = ['car', 'bus']
-        locations = ['vehlane', 'outgolane', 'incomlane', 'jun', 'parking']
-        statuses = ['broke', 'incotlft', 'incotrht', 'hozlit']
+        classes = ['c', 'b']
+        locations = ['v', 'o', 'i', 'j', 'p']
+        statuses = ['br', 'lft', 'rht', 'h']
 
         for image_file in os.listdir(img_path):
             if image_file.endswith('.jpg') or image_file.endswith('.png'):
                 image_path = os.path.join(img_path, image_file)
+                mask_image = os.path.join(mask_path, image_file)
                 label_path = os.path.join(new_txt_path, image_file.replace('.jpg', '.txt').replace('.png', '.txt'))
 
                 image = cv2.imread(image_path)
+                mask = cv2.imread(mask_image)
                 h, w, _ = image.shape
+
+                colorstep = numpy.array([60, 80, 110], dtype=numpy.uint8)
+                mask *= colorstep
 
                 with open(label_path, 'r') as file:
                     lines = file.readlines()
@@ -45,6 +52,8 @@ for subfolder in os.listdir(base_folder):
                             label += f" | {statuses[idx]}"
 
                     cv2.putText(image, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 255, 0), 1)
+
+                image = image//2 + mask//2
 
                 output_path = os.path.join(cleansing_path, image_file)
                 cv2.imwrite(output_path, image)
